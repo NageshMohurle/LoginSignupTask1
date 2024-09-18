45bda2001e21c13be4ca844993d28a917fe539a9
Nageshwar Mohurle <103045675+NageshMohurle@users.noreply.github.com>
2024-09-19 00:35:40 +0530

    change routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, RouterOutlet, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/singup/singup.component';
 import { AppComponent } from './app.component';
@@ -7,7 +7,8 @@ import { AppComponent } from './app.component';
 const routes: Routes = [
   { path: "login", component: LoginComponent },
   { path: "sign-up", component: SignupComponent },
-  { path: "", component: AppComponent }
+  { path: "", component: RouterOutlet },
+  { path: '**', redirectTo: 'login' }
 ];
 
 @NgModule({
